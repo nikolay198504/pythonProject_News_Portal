b045e6f3108353327552ce65b8c06b264a66f54b
Name <kolupaev_nikolay85@gmaile.ru>
2024-06-05 22:07:31 +0300

    go2

--- News_Portal/News_Portal/settings.py
+++ News_Portal/News_Portal/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 
+load_dotenv(find_dotenv())
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -226,3 +228,5 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # Яндекс использует ssl, включать его здесь обязательно
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
